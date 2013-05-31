2bda3e09fd1532dd3b8125fba97a6029a0153665
Ondrej Sika <sika.ondrej@gmail.com>
2013-05-31 16:22:27 +0200

    add south migration support, v.1.3.3

--- large_data_admin/db_fields.py
+++ large_data_admin/db_fields.py
@@ -15,6 +15,12 @@ class ManyToManyField(models.ManyToManyField):
         defaults.update(kwargs)
         return super(ManyToManyField, self).formfield(**defaults)
 
+    def south_field_triple(self):
+        from south.modelsinspector import introspector
+        field_class = "django.db.models.fields.ManyToManyField"
+        args, kwargs = introspector(self)
+        return (field_class, args, kwargs)
+
 class ForeignKey(models.ForeignKey):
     def __init__(self, to, **kwargs):
         self.model_str = u"%s.%s"%(to.__module__, to.__name__)
@@ -27,3 +33,9 @@ class ForeignKey(models.ForeignKey):
         }
         defaults.update(kwargs)
         return super(ForeignKey, self).formfield(**defaults)
+
+    def south_field_triple(self):
+        from south.modelsinspector import introspector
+        field_class = "django.db.models.fields.ForeignKey"
+        args, kwargs = introspector(self)
+        return (field_class, args, kwargs)
