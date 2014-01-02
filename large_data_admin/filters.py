2a5601ff928c6aeefa6863a362c6a162aea07f34
Ondrej Sika <ondrej@ondrejsika.com>
2014-01-02 18:48:32 +0100

    rm ajax filter attribute

--- large_data_admin/filters.py
+++ large_data_admin/filters.py
@@ -22,22 +22,3 @@ def get_ajax_filter(filter_title, model, field):
         def queryset(self, request, queryset):
             pass
     return AjaxFilter
-
-def get_ajax_attribute_filter(filter_title, model, field):
-    class AjaxFilter(admin.SimpleListFilter):
-        parameter_name = 'decade'
-        title = _(filter_title)
-        template = "large_data_admin/filter/ajax_filter_attribute.html"
-        def __init__(self, *args, **kwargs):
-            self.random = "a%s" % random.randint(11111111, 99999999)
-            self.STATIC_URL = settings.STATIC_URL
-            self.model = "%s.%s" % (model.__module__, model.__name__)
-            self.field = field
-            super(AjaxFilter, self).__init__(*args, **kwargs)
-        def has_output(self):
-            return True
-        def lookups(self, request, model_admin):
-            pass
-        def queryset(self, request, queryset):
-            pass
-    return AjaxFilter
