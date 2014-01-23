d776af966145eafb251040dfef1ef964661d0216
Ondrej Sika <ondrej@ondrejsika.com>
2014-01-23 16:33:34 +0100

    update version v1.13.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-large-data-admin",
-    version = "1.12.0",
+    version = "1.13.0",
     url = 'https://github.com/sikaondrej/django-large-data-admin/',
     license = 'MIT',
     description = "",
